d11353d2cc5215ca1acbc8285a300b2b3e3e529c
Rosa-musi <nadamejor27@hotmail.com>
2021-08-25 09:39:36 -0500

    nodos

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,55 +1,90 @@
-import { home } from "./views/Home";
-import { SignUp } from "./views/SignUp";
-
-export default const login = {
-    template: () => {
-        const view = `
-  <div class="acount">
-  <div class="acount-header">
-  <img src="../img/logoFormLoveBook.png" class="acount-logo" alt="LoveBook logo">
-  </div>
-  <div class="log-content">
-  <div class="login-user">
-      <form action="" id="login-form" class="form">
-          <label for="login-email">Email</label>
-          <input type="email" id="login-email" required>
-          <label for="login-password">Password</label>
-          <input type="password" id="login-password" required>
-          <button type="submit" id="btnLogin">Login</button>
-      </form>
-  </div>
-  <div class="division">
-      <hr class="division-1">
-      <p class="division-2">O</p>
-      <hr class="division-3">
-  </div>
-  <div class="login-google">
-      <img src="../img/icongoogle.png" alt="logo google" class="logo-google">
-      <p>Login with google</p>
-  </div>
-  <div class="pieform">
-      <p style="font-size: 12px;">You do not have an account</p>
-      <button id="signup">Sign Up</button>
-  </div>
-  </div>
-  </div>`
-  }};
-  
-    const btnLogin = document.getElementById('btnLogin');
-    const btnSignUp = document.getElementById('signup');
+export const Login = () => {
+    //div principal
+    const acountDiv = document.createElement('div')
+    acountDiv.classList.add('acount-header')
     
-    btnLogin.addEventListener('click', (e) => {
-        e.preventDefault();
-        router.onNavigate('/home');
-        home.template();
-    }); 
+    //header
+    const headerDiv = document.createElement('div')
+    headerDiv.classList.add('acount-header')
+    const imgHeader = document.createElement('img')
+    imgHeader.setAttribute("class", "acount-logo")
+    imgHeader.setAttribute("src", "../img/logoFormLoveBook.png")
+    imgHeader.setAttribute("alt", "LoveBook logo")
+    headerDiv.appendChild(imgHeader)
+    acountDiv.appendChild(headerDiv)
 
+    //formulario
+    const formDiv = document.createElement('div')
+    formDiv.setAttribute("class", "login-user")
+    const loginForm = document.createElement('form')
+    loginForm.setAttribute("id", "login-form")
+    loginForm.setAttribute("class", "form")
+    const labelEmail = document.createElement('label')
+    labelEmail.textContent = "Email"    
+    labelEmail.setAttribute("for", "login-email")
+    loginForm.appendChild(labelEmail)
+    const inputEmail = document.createElement('input')
+    inputEmail.setAttribute("type", "email")
+    inputEmail.setAttribute("id", "login-email")
+    inputEmail.required = true
+    loginForm.appendChild(inputEmail)
+    const labelPassword = document.createElement('label')
+    labelPassword.textContent = "Password"
+    labelPassword.setAttribute("for", "login-password")
+    loginForm.appendChild(labelPassword)
+    const inputPassword = document.createElement('input')
+    inputPassword.setAttribute("type", "password")
+    inputPassword.setAttribute("id", "login-password")
+    inputPassword.required = true
+    loginForm.appendChild(inputPassword)
+    const loginButton = document.createElement('button')
+    loginButton.innerText = "Login"
+    loginButton.setAttribute("type", "submit")
+    loginButton.setAttribute("id", "btnLogin")
+    loginForm.appendChild(loginButton)
+    formDiv.appendChild(loginForm)
+    acountDiv.appendChild(formDiv)
 
-    btnSignUp.addEventListener('click', (e) => {
-        e.preventDefault();
-        router.onNavigate('/signUp');
-        signUp.template();
-    });
+    //división
+    const divisionDiv = document.createElement('div')
+    divisionDiv.setAttribute("class", "division")
+    const lineaUno = document.createElement("hr")
+    lineaUno.setAttribute("class", "division-1")
+    divisionDiv.appendChild(lineaUno)
+    const o = document.createElement("p")
+    o.innerText = "O"
+    o.setAttribute("class", "division-2")
+    divisionDiv.appendChild(o)
+    const lineaDos = document.createElement("hr")
+    lineaDos.setAttribute("class", "division-3")
+    divisionDiv.appendChild(lineaDos)
+    acountDiv.appendChild(divisionDiv)
 
+    //loguearse con google
+    const loginGoogle = document.createElement('div')
+    loginGoogle.setAttribute("class", "login-google")
+    const googleLogo = document.createElement('img')
+    googleLogo.setAttribute("src", "../img/icongoogle.png")
+    googleLogo.setAttribute("alt", "logo google")
+    googleLogo.setAttribute("class", "logo-google")
+    loginGoogle.appendChild(googleLogo)
+    const textGoogleLogin = document.createElement('p')
+    textGoogleLogin.innerText = "Login with google"
+    loginGoogle.appendChild(textGoogleLogin)
+    acountDiv.appendChild(loginGoogle)
 
+    //pieform 
+    const pieForm = document.createElement('div')
+    pieForm.setAttribute("class", "pieform")
+    const textPieForm = document.createElement('p')
+    textPieForm.innerText = "You do not have an account"
+    textPieForm.setAttribute("class", "text-pieform")
+    pieForm.appendChild(textPieForm)
+    const pieFormButton = document.createElement('button')
+    pieFormButton.innerText = "Sign Up"
+    pieFormButton.setAttribute("id", "signup")
+    pieForm.appendChild(pieFormButton)
+    acountDiv.appendChild(pieForm)
 
+    return acountDiv;
+  };                             
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,35 +1,17 @@
-import { Login } from './views/Login.js';
-import { SignUp } from './views/SignUp.js';
-import { Profile } from './views/Profile.js';
-import { Home } from './views/Home.js';
+ 
 
-export const routes = {
-    '/': Login,
-    '/signUp': SignUp,
-    '/profile': Profile,
-    '/home': Home,
-};
+const rootDiv = document.getElementById('root');
 
-const main = document.getElementById('templates');
 export const onNavigate = (pathname) => {
-    window.history.pushState({},
-        pathname,
-        window.location.origin + pathname,
+    window.history.pushState(
+      {},
+      pathname,
+      window.location.origin + pathname,
     );
-    while (main.firstChild) {
-        main.removeChild(main.firstChild);
+  
+    while (rootDiv.innerHTML != "") {
+      rootDiv.innerHTML = ""
     }
-    main.innerHTML = (routes[pathname]());
-};
-console.log(10, routes)
-if (routes.hasOwnProperty(window.location.pathname)) {
-    const component = routes[window.location.pathname];
-    window.onpopstate = () => {
-        while (main.firstChild) {
-            main.removeChild(rootDiv.firstChild);
-        }
-        main.innerHTML = (routes[window.location.pathname]());
-
-    };
-    main.innerHTML = (component());
-}
+  
+    rootDiv.innerHTML(routes[pathname]());
+  };
